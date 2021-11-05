00d659ae941879a94bfeba9d92b6b70a84ccc6bf
karthik G <karthikg164@gmail.com>
2021-11-05 14:22:05 +0530

    Hello world from scratch

--- bootloader.asm
+++ bootloader.asm
@@ -1,3 +1,27 @@
+; Print Hello world on screen
+
+mov ah, 0x0e
+mov al, 'h'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'w'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al,'r'
+int 0x10
+mov al,'l'
+int 0x10
+mov al,'d'
+int 0x10
+
 jmp $
 
 times 510 - ($-$$) db 0
